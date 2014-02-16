04f16c9cdf06e03f611d11a6c8221b50739132e4
endisnear <endisnear@abv.bg>
2014-02-16 19:46:44 +0200

    Untabified all the files.

--- Huffman/BinaryTree.hpp
+++ Huffman/BinaryTree.hpp
@@ -4,127 +4,127 @@
 template <class T>
 class BinaryTree
 {
-	struct TreeNode
-	{
-		TreeNode(const T& _val, TreeNode* _pLeft = NULL, TreeNode* _pRight = NULL)
-		{
-			val = _val;
-			pLeft = _pLeft;
-			pRight = _pRight;
-		}
-		T val;
-		TreeNode* pLeft;
-		TreeNode* pRight;
-	};
+    struct TreeNode
+    {
+        TreeNode(const T& _val, TreeNode* _pLeft = NULL, TreeNode* _pRight = NULL)
+        {
+            val = _val;
+            pLeft = _pLeft;
+            pRight = _pRight;
+        }
+        T val;
+        TreeNode* pLeft;
+        TreeNode* pRight;
+    };
 
-	class BinTreeIter
-	{
-		TreeNode* ptr;
-	public:
-		BinTreeIter(TreeNode* node)
-		{
-			this->ptr = node;
-		}
+    class BinTreeIter
+    {
+        TreeNode* ptr;
+    public:
+        BinTreeIter(TreeNode* node)
+        {
+            this->ptr = node;
+        }
 
-		BinTreeIter operator++()
-		{
-			return this->ptr = this->ptr->pLeft;
-		}
-		BinTreeIter operator++(int)
-		{
-			return this->ptr = this->ptr->pRight;
-		}
-		T& operator*() const
-		{
-			return this->ptr->val;
-		}
-		operator bool() const
-		{
-			return this->ptr != NULL;
-		}
-		bool operator== (BinTreeIter& right) const
-		{
-			return this == *right;
-		}
-		bool operator!= (BinTreeIter& right) const
-		{
-			return this != *right;
-		}
-	};
+        BinTreeIter operator++()
+        {
+            return this->ptr = this->ptr->pLeft;
+        }
+        BinTreeIter operator++(int)
+        {
+            return this->ptr = this->ptr->pRight;
+        }
+        T& operator*() const
+        {
+            return this->ptr->val;
+        }
+        operator bool() const
+        {
+            return this->ptr != NULL;
+        }
+        bool operator== (BinTreeIter& right) const
+        {
+            return this == *right;
+        }
+        bool operator!= (BinTreeIter& right) const
+        {
+            return this != *right;
+        }
+    };
 
-	TreeNode* createNode(const T& _val)
-	{
-		TreeNode* tmp = new TreeNode();
-		if (!tmp)
-		{
-			return NULL;
-		}
-		tmp->val = _val;
-		return tmp;
-	}
+    TreeNode* createNode(const T& _val)
+    {
+        TreeNode* tmp = new TreeNode();
+        if (!tmp)
+        {
+            return NULL;
+        }
+        tmp->val = _val;
+        return tmp;
+    }
 
-	TreeNode* root;
+    TreeNode* root;
 public:
-	BinaryTree()
-	{
-		this->root = NULL;
-	}
-	~BinaryTree()
-	{
-		deleteNode(root);
-	}
+    BinaryTree()
+    {
+        this->root = NULL;
+    }
+    ~BinaryTree()
+    {
+        deleteNode(root);
+    }
 
-	BinTreeIter& GetIter()const
-	{
-		return BinTreeIter(root);
-	}
+    BinTreeIter& GetIter()const
+    {
+        return BinTreeIter(root);
+    }
 
-	void deleteNode(TreeNode* node)
-	{
-		if (node->pLeft)
-		{
-			deleteNode(node->pLeft);
-		}
-		if (node->pRight)
-		{
-			deleteNode(node->pRight);
-		}
-		delete node;
-	}
+    void deleteNode(TreeNode* node)
+    {
+        if (node->pLeft)
+        {
+            deleteNode(node->pLeft);
+        }
+        if (node->pRight)
+        {
+            deleteNode(node->pRight);
+        }
+        delete node;
+    }
 
-	bool initRoot(const T& val)
-	{
-		if (!root = createNode(val))
-		{
-			return false;
-		}
-		return true;
-	}
+    bool initRoot(const T& val)
+    {
+        if (!root = createNode(val))
+        {
+            return false;
+        }
+        return true;
+    }
 
-	bool adoptLeft(BinTreeIter* iter, const T& val)
-	{
-		if ((*iter)->pLeft)
-		{
-			deleteNode((*iter)->pLeft);
-		}
-		if (!(*iter)->pLeft = createNode(val))
-		{
-			return false;
-		}
-		return true;
-	}
-	bool adoptRight(BinTreeIter* iter, const T& val)
-	{
-		if ((*iter)->pRight)
-		{
-			deleteNode((*iter)->pRight);
-		}
-		if (!(*iter)->pRight = createNode(val))
-		{
-			return false;
-		}
-		return true;
-	}
+    bool adoptLeft(BinTreeIter* iter, const T& val)
+    {
+        if ((*iter)->pLeft)
+        {
+            deleteNode((*iter)->pLeft);
+        }
+        if (!(*iter)->pLeft = createNode(val))
+        {
+            return false;
+        }
+        return true;
+    }
+    bool adoptRight(BinTreeIter* iter, const T& val)
+    {
+        if ((*iter)->pRight)
+        {
+            deleteNode((*iter)->pRight);
+        }
+        if (!(*iter)->pRight = createNode(val))
+        {
+            return false;
+        }
+        return true;
+    }
 };
 
 #endif //BINARY_TREE_HPP
--- Huffman/main.cpp
+++ Huffman/main.cpp
@@ -4,29 +4,29 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 4)
-	{
-		std::cout << "Too few arguments!" << std::endl;
-	}
-	else if (argc > 4)
-	{
-		std::cout << "Too many arguments!" << std::endl;
-	}
-	else
-	{
-		if (!strcmp(argv[1], "-a")) //arhivirane
-		{
-			archive(argv[2], argv[3]);
-			
-		}
-		else if(!strcmp(argv[1], "-u"))//razarhivirane
-		{
-			extract(argv[2], argv[3]);
-		}
-		else //error
-		{
-			std::cout << "1st argument should be \"-a\" or \"-u\"!" << std::endl;
-		}
-	}
-	return 0;
+    if (argc < 4)
+    {
+        std::cout << "Too few arguments!" << std::endl;
+    }
+    else if (argc > 4)
+    {
+        std::cout << "Too many arguments!" << std::endl;
+    }
+    else
+    {
+        if (!strcmp(argv[1], "-a")) //arhivirane
+        {
+            archive(argv[2], argv[3]);
+            
+        }
+        else if(!strcmp(argv[1], "-u"))//razarhivirane
+        {
+            extract(argv[2], argv[3]);
+        }
+        else //error
+        {
+            std::cout << "1st argument should be \"-a\" or \"-u\"!" << std::endl;
+        }
+    }
+    return 0;
 }
