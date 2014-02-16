50ff2e2711ca10e8a2192ea669d72754a8fb2dac
endisnear <endisnear@abv.bg>
2014-02-16 19:43:53 +0200

    Added Binary Tree and main function.

---
+++ Huffman/Utils.cpp
@@ -0,0 +1,34 @@
+#include "Utils.h"
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <iostream>
+#include <stdio.h>
+#include "BinaryTree.hpp"
+bool archive(const char * inputName, const char* outputName)
+{
+	FILE * input,* output;
+	input = fopen(inputName, "rb");
+	if (!input)
+	{
+		std::cout << "Cannot open file for reading!" << std::endl;
+		return false;
+	}
+
+	output = fopen(outputName, "rb");
+	if (!output)
+	{
+		std::cout << "Cannot open file for writing!" << std::endl;
+		return false;
+	}
+
+
+	fclose(input);
+	fclose(output);
+	return true;
+}
+
+bool extract(const char* input, const char* result)
+{
+
+	return false;
+}
---
+++ Huffman/Utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+bool archive(const char * input, const char* output);
+
+bool extract(const char* input, const char* result);
+
+#endif
